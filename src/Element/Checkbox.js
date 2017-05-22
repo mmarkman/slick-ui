9933dce1816c2d15b9727197372bf579e69244c1
mmarkman <mmarkman@users.noreply.github.com>
2017-05-21 21:32:33 -0400

    here's the check!

--- src/Element/Checkbox.js
+++ src/Element/Checkbox.js
@@ -1,162 +1,140 @@
-SlickUI.namespace('Element');
+import Element from './Element'
 
-/**
- * Checkboxes can be toggled on/off. Use the third
- * parameter to specify whether to use a checkbox,
- * radio or cross sprite. Use the defined constants
- * to do so.
- *
- * @author Richard Snijders <richard@fizz.nl>
- * @param x
- * @param y
- * @param type
- * @constructor
- */
-SlickUI.Element.Checkbox = function (x, y, type) {
-    this._x = x;
-    this._y = y;
-    this.container = null;
-    this._checked = false;
+export class Checkbox extends Element {
+
+  constructor (x, y, type) {
+    super()
+
+    // some constants
+    this.TYPE_CHECKBOX = 0
+    this.TYPE_RADIO = 1
+    this.TYPE_CROSS = 2
+
+    this._x = x
+    this._y = y
+    this.container = null
+    this._checked = false
     this.type = type
-
-    if(typeof type == 'undefined') {
-        this.type = SlickUI.Element.Checkbox.TYPE_CHECKBOX;
+    if (typeof type === 'undefined') {
+      this.type = this.TYPE_CHECKBOX
     }
-};
 
-SlickUI.Element.Checkbox.TYPE_CHECKBOX = 0;
-SlickUI.Element.Checkbox.TYPE_RADIO = 1;
-SlickUI.Element.Checkbox.TYPE_CROSS = 2;
+    Object.defineProperty(this, 'x', {
+      get: function () {
+        return this.displayGroup.x + this._x
+      },
+      set: function (value) {
+        this.displayGroup.x = value - this._x
+      }
+    })
 
-/**
- * Internal Container handling.
- *
- * @param container
- */
-SlickUI.Element.Checkbox.prototype.setContainer = function (container) {
-    this.container = container;
-};
+    Object.defineProperty(this, 'y', {
+      get: function () {
+        return this.displayGroup.y + this._y
+      },
+      set: function (value) {
+        this.displayGroup.y = value - this._y
+      }
+    })
 
-/**
- * Initializer
- */
-SlickUI.Element.Checkbox.prototype.init = function() {
-    var x = this.container.x + this._x;
-    var y = this.container.y + this._y;
+    Object.defineProperty(this, 'visible', {
+      get: function () {
+        return this.sprite.visible
+      },
+      set: function (value) {
+        this.sprite.visible = value
+      }
+    })
 
-    var key;
-    switch(this.type) {
-        case SlickUI.Element.Checkbox.TYPE_RADIO:
-            key = 'radio';
-            break;
-        case SlickUI.Element.Checkbox.TYPE_CROSS:
-            key = 'cross';
-            break;
-        default:
-            key = 'check';
-            break;
+    Object.defineProperty(this, 'alpha', {
+      get: function () {
+        return this.sprite.alpha
+      },
+      set: function (value) {
+        this.sprite.alpha = value
+      }
+    })
+
+    Object.defineProperty(this, 'events', {
+      get: function () {
+        return this.sprite.events
+      }
+    })
+
+    Object.defineProperty(this, 'input', {
+      get: function () {
+        return this.sprite.input
+      }
+    })
+
+    Object.defineProperty(this, 'width', {
+      get: function () {
+        return this.sprite.width
+      },
+      set: function (value) {
+        this.sprite.width = value
+      }
+    })
+
+    Object.defineProperty(this, 'height', {
+      get: function () {
+        return this.sprite.height
+      },
+      set: function (value) {
+        this.sprite.height = value
+      }
+    })
+
+    Object.defineProperty(this, 'checked', {
+      get: function () {
+        return this._checked
+      },
+      set: function (value) {
+        this._checked = value
+        if (value) {
+          this.sprite.loadTexture(this._spriteOn.texture)
+        } else {
+          this.sprite.loadTexture(this._spriteOff.texture)
+        }
+      }
+    })
+  }
+
+  setContainer (container) {
+    this.container = container
+  };
+
+  init () {
+    var x = this.container.x + this._x
+    var y = this.container.y + this._y
+
+    var key
+    switch (this.type) {
+      case this.TYPE_RADIO:
+        key = 'radio'
+        break
+      case this.TYPE_CROSS:
+        key = 'cross'
+        break
+      default:
+        key = 'check'
+        break
     }
-    var sprites = this.container.root.getRenderer('checkbox').render(key);
-    this.sprite = this.container.root.game.make.sprite(0,0,sprites[0].texture);
-    this.sprite.x = x;
-    this.sprite.y = y;
-    this._spriteOff = sprites[0];
-    this._spriteOn = sprites[1];
-    this.displayGroup = this.container.root.game.add.group();
-    this.displayGroup.add(this.sprite);
-    this.container.displayGroup.add(this.displayGroup);
-    this.sprite.inputEnabled = true;
-    this.sprite.fixedToCamera = true;
-    this.input.useHandCursor = true;
+    var sprites = this.container.root.getRenderer('checkbox').render(key)
+    this.sprite = this.container.root.game.make.sprite(0, 0, sprites[0].texture)
+    this.sprite.x = x
+    this.sprite.y = y
+    this._spriteOff = sprites[0]
+    this._spriteOn = sprites[1]
+    this.displayGroup = this.container.root.game.add.group()
+    this.displayGroup.add(this.sprite)
+    this.container.displayGroup.add(this.displayGroup)
+    this.sprite.inputEnabled = true
+    this.sprite.fixedToCamera = true
+    this.input.useHandCursor = true
 
     this.events.onInputDown.add(function () {
-        this.checked = !this.checked;
-    }, this);
-};
-
-
-/* ------------------------------- */
-
-
-/**
- * Setters / getters
- */
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'x', {
-    get: function() {
-        return this.displayGroup.x + this._x;
-    },
-    set: function(value) {
-        this.displayGroup.x = value - this._x;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'y', {
-    get: function() {
-        return this.displayGroup.y + this._y;
-    },
-    set: function(value) {
-        this.displayGroup.y = value - this._y;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'checked', {
-    get: function() {
-        return this._checked;
-    },
-    set: function(value) {
-        this._checked = value;
-        if(value) {
-            this.sprite.loadTexture(this._spriteOn.texture);
-        } else {
-            this.sprite.loadTexture(this._spriteOff.texture);
-        }
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'visible', {
-    get: function() {
-        return this.sprite.visible;
-    },
-    set: function(value) {
-        this.sprite.visible = value;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'alpha', {
-    get: function() {
-        return this.sprite.alpha;
-    },
-    set: function(value) {
-        this.sprite.alpha = value;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'events', {
-    get: function() {
-        return this.sprite.events;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'input', {
-    get: function() {
-        return this.sprite.input;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'width', {
-    get: function() {
-        return this.sprite.width;
-    },
-    set: function(value) {
-        this.sprite.width = value;
-    }
-});
-
-Object.defineProperty(SlickUI.Element.Checkbox.prototype, 'height', {
-    get: function() {
-        return this.sprite.height;
-    },
-    set: function(value) {
-        this.sprite.height = value;
-    }
-});
+      this.checked = !this.checked
+    }, this)
+  }
+}
