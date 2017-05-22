047a606de390aaf2dd5b22651b52c4091d1fbbfb
tymerica <COOL-DOGS@Tylers-MacBook-Pro.local>
2017-05-21 20:23:06 -0400

    did all them renderers es6

--- src/Element/Renderer/SliderRenderer.js
+++ src/Element/Renderer/SliderRenderer.js
@@ -1,67 +1,63 @@
-SlickUI.namespace('Element.Renderer');
+export class SliderRenderer {
 
-/**
- * Default slider renderer
- *
- * @author Richard Snijders <richard@fizz.nl>
- * @constructor
- */
-SlickUI.Element.Renderer.SliderRenderer = function(game) { this.game = game };
+  constructor (game) {
+    this.game = game
+  }
 
-/**
- * Renders the slider and returns it's components as an array
- *
- * @returns Array (0: base; 1: handle off; 2: handle on)
- */
-SlickUI.Element.Renderer.SliderRenderer.prototype.render = function(size, vertical) {
-    var theme = this.game.cache.getJSON('slick-ui-theme');
+    /**
+     * Renders the slider and returns it's components as an array
+     *
+     * @returns Array (0: base; 1: handle off; 2: handle on)
+     */
+  render (size, vertical) {
 
-    var sprite_base = this.game.make.sprite(0, 0, 'slick-ui-slider_base');
-    var sprite_end = this.game.make.sprite(0, 0, 'slick-ui-slider_end');
+    var spriteBase = this.game.make.sprite(0, 0, 'slick-ui-slider_base')
+    var spriteEnd = this.game.make.sprite(0, 0, 'slick-ui-slider_end')
 
-    var bmd = this.game.add.bitmapData(size, sprite_end.height);
+    var bmd = this.game.add.bitmapData(size, spriteEnd.height)
     bmd.copy(
-        sprite_base,
-        0,
-        0,
-        1,
-        sprite_base.height,
-        0,
-        Math.round(sprite_end.height/4),
-        size,
-        sprite_base.height
-    );
+            spriteBase,
+            0,
+            0,
+            1,
+            spriteBase.height,
+            0,
+            Math.round(spriteEnd.height / 4),
+            size,
+            spriteBase.height
+        )
     bmd.copy(
-        sprite_end,
-        0,
-        0,
-        sprite_end.width,
-        sprite_end.height,
-        0,
-        0,
-        sprite_end.width,
-        sprite_end.height
-    );
+            spriteEnd,
+            0,
+            0,
+            spriteEnd.width,
+            spriteEnd.height,
+            0,
+            0,
+            spriteEnd.width,
+            spriteEnd.height
+        )
     bmd.copy(
-        sprite_end,
-        0,
-        0,
-        sprite_end.width,
-        sprite_end.height,
-        size - sprite_end.width,
-        0,
-        sprite_end.width,
-        sprite_end.height
-    );
+            spriteEnd,
+            0,
+            0,
+            spriteEnd.width,
+            spriteEnd.height,
+            size - spriteEnd.width,
+            0,
+            spriteEnd.width,
+            spriteEnd.height
+        )
 
-    var handle_off = this.game.make.sprite(0, 0, 'slick-ui-slider_handle_off');
-    var handle_on = this.game.make.sprite(0, 0, 'slick-ui-slider_handle_on');
+    var handleOff = this.game.make.sprite(0, 0, 'slick-ui-slider_handle_off')
+    var handleOn = this.game.make.sprite(0, 0, 'slick-ui-slider_handle_on')
 
-    sprite_base = this.game.make.sprite(0, 0, bmd);
+    spriteBase = this.game.make.sprite(0, 0, bmd)
 
-    if(vertical) {
-        sprite_base.angle = 90;
+    if (vertical) {
+      spriteBase.angle = 90
     }
 
-    return [sprite_base, handle_off, handle_on];
-};
+    return [spriteBase, handleOff, handleOn]
+  };
+}
